e33fafd0b9b7b2a2917e4859289cbc3ef42600d5
Walber Vaz <wvs.walber@gmail.com>
2024-10-23 17:07:03 -0300

    refactor(chapter_one): improve HelloWorld function
    Refactor the HelloWorld function in the chapter_one/hello_world.go file to provide more detailed comments and explanations. The function now includes comments that describe the usage of the fmt.Println function, the variadic parameter, the empty interface, and the usage of the blank identifier. Additionally, the function now prints the number of bytes written to the console.

--- chapter_one/hello_world.go
+++ chapter_one/hello_world.go
@@ -4,5 +4,20 @@ import "fmt"
 
 // HelloWorld prints "Hello, World!" to the console.
 func HelloWorld() {
-	fmt.Println("Hello, World!")
+	// function Println from the fmt package is used to print to the console
+	// https://pkg.go.dev/fmt#Println
+
+	// Println formats using the default formats for its operands and writes to standard output.
+	// Spaces are always added between operands and a newline is appended. It returns the number of bytes written and any write error encountered.
+
+	// ... is a variadic parameter that allows the function to accept any number of arguments
+	// https://golang.org/ref/spec#Passing_arguments_to_..._parameters
+
+	// Any types implement interface{} which is the empty interface
+	// https://golang.org/ref/spec#Interface_types
+
+	// Not variable usage _ is used to ignore the return value
+	// https://golang.org/ref/spec#Blank_identifier
+	anyBytesWritten, _ := fmt.Println("Hello, World!")
+	fmt.Println(anyBytesWritten)
 }
