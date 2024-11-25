ac228360f55aec3e1ae10f2c867803f1d9073461
Walber Vaz <wvs.walber@gmail.com>
2024-11-25 11:21:49 -0300

    feat(chapter_one): Add PackageFmt function to demonstrate string literals and formatting

---
+++ chapter_one/pkg_fmt.go
@@ -0,0 +1,37 @@
+package chapter_one
+
+import (
+	"fmt"
+	"strings"
+)
+
+// PackageFmt demonstrates the difference between interpreted and raw string literals.
+// Interpreted string literals are character sequences between double quotes "".
+// Raw string literals are character sequences between backticks “.
+func PackageFmt() {
+	// Interpreted string literals
+	// Interpreted string literals are character sequences between double quotes "".
+	interpretedStringLiterals := "Usage Interpreted string literals: Hello, World!\nStudents are learning Go!"
+
+	// Raw string literals
+	// Raw string literals are character sequences between backticks ``.
+	rawStringLiterals := `Usage Raw string literals: Hello, World!\nStudents are learning Go!`
+
+	fmt.Println(interpretedStringLiterals)
+	fmt.Println(rawStringLiterals)
+
+	// Sprint
+	// Sprint formats using the default formats for its operands and returns the resulting string.
+	usageSprint := fmt.Sprint("Usage Sprint: Hello, World!\nStudents are learning Go!")
+	fmt.Println(usageSprint)
+
+	// Fprint
+	// Fprint formats using the default formats for its operands and writes to w.
+	var buffer strings.Builder
+	_, err := fmt.Fprint(&buffer, "Usage Fprint: Hello, World!\nStudents are learning Go!")
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println(buffer.String())
+	}
+}
